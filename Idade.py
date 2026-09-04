df8dd4b24fb24bf24f6d819649a13703d69cc069
sguerri-dev <raphaelsguerri60@gmail.com>
2026-09-04 04:05:17 -0300

    Create Idade.py

---
+++ Idade.py
@@ -0,0 +1,9 @@
+nome = input ("Qual o seu nome?")
+print ("Ola" , nome)
+idade = int(input("Quantos anos voce tem?."))
+if idade >= 18:
+   print ( "Voce é maior de idade")
+else:
+   print ( "Voce é menor de idade")
+if idade >= 60: 
+   print ( "você é um idoso")
