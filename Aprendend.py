f6fb3e8192626ebf40f288b67b2ecb6be1a64ebf
sguerri-dev <raphaelsguerri60@gmail.com>
2026-09-05 01:43:02 -0300

    Operações.py

---
+++ Aprendend.py
@@ -0,0 +1,9 @@
+idade = int(input( "qual sua idade?"))
+       if idade < 12:
+           print ("Você é um/a criança.") 
+       elif idade < 18:
+            print ("Você é um/a adolescente.")
+       elif idade <= 64:
+           print ("Você é um/a adulto/a.")
+       else: 
+            print ("Você é um/a idoso/a.")
